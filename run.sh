54ae96412e5eee0a1d15feb5c5775166684f02c8
Javier G. Sogo <jgsogo@gmail.com>
2018-11-23 10:21:09 +0100

    run example

---
+++ run.sh
@@ -0,0 +1,42 @@
+conan remove libA -f
+conan remove libB -f
+
+printf "\n\n********* libA/1.0 **********\n\n"
+
+cd libA
+conan create . libA/1.0@demo/test
+conan alias libA/1.latest@demo/test libA/1.0@demo/test
+conan upload libA --all --confirm -r $1
+
+printf "\n\n******** libB/1.0 ***********\n\n"
+
+cd ../libB
+conan create . libB/1.0@demo/test
+conan alias libB/1.latest@demo/test libB/1.0@demo/test
+conan upload libB --all --confirm -r $1
+
+conan remove libB -f
+conan remove libA -f
+
+printf "\n\n******** install . **********\n\n"
+
+cd ../project
+conan install .
+
+printf "\n\n******** libA/1.1 ***********\n\n"
+
+cd ../libA
+conan create . libA/1.1@demo/test
+
+printf "\n\n******** libB/1.1 ***********\n\n"
+
+cd ../libB
+conan create . libB/1.1@demo/test
+conan alias libB/1.latest@demo/test libB/1.1@demo/test
+conan alias libA/1.latest@demo/test libA/1.1@demo/test
+
+printf "\n\n******** install . -u ********\n\n"
+
+cd ../project
+conan install . -u
+
